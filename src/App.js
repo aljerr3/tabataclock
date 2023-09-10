cdf48d4748cae769809a169366e43114f8d093f3
aljerr3 <aljerr3@gmail.com>
2023-09-09 22:04:40 -0300

    fix colors a css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Tabata from './Tabata';
+import Referidos from './Referidos';
 
 function App() {
   return (
     <div >
       <Tabata />
+      <Referidos/>
     </div>
   );
 }
---
+++ src/Referidos.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Referidos() {
+  return (
+    <div className='referidos'>
+        <li>Amazon</li>
+        <li>Aliexpress</li>
+        <li>Wish</li>
+        <li>Shein</li>
+        <li>Walmart</li>
+        <li>Bestbuy</li>
+        <li>Target</li>
+    </div>
+  )
+}
+
+export default Referidos
