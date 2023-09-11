837b3118642f78f8ae083ae6a0788072e053c7f3
aljerr3 <aljerr3@gmail.com>
2023-09-11 18:53:26 -0300

    fix colors a css

--- src/Seo.jsx
+++ src/Seo.jsx
@@ -4,19 +4,16 @@ function Seo() {
   return (
     <div>
       <div className="seo">
-      El entrenamiento Tabata es una forma
-      de entrenamiento interválico de alta intensidad (HIIT) que consiste en 8
-      rondas de ejercicios. Cada ronda tiene una duración de 4 minutos, dividida
-      en 30 segundos de ejercicio intenso seguido de 10 segundos de descanso.
-      Este método, diseñado por el Dr. Izumi Tabata, ha demostrado ser
-      extremadamente efectivo para mejorar tanto la capacidad aeróbica como
-      anaeróbica, además de ayudar en la quema de grasa en un corto período de
-      tiempo. Con nuestro reloj Tabata online, te ofrecemos una herramienta
-      intuitiva y fácil de usar, diseñada especialmente para guiarte a través de
-      cada sesión de entrenamiento. 
-
-    </div>
-
+        El entrenamiento Tabata es una forma de entrenamiento interválico de
+        alta intensidad (HIIT) que generalmente consiste en 8 rondas de
+        ejercicios. Existen algunas variaciones, pero normalmente cada ronda
+        tiene una duración de 30 segundos. Este método, diseñado por el Dr.
+        Izumi Tabata, ha demostrado ser extremadamente efectivo para mejorar
+        tanto la capacidad aeróbica como anaeróbica, además de ayudar en la
+        quema de grasa en un corto período de tiempo. Con nuestro reloj Tabata
+        online, te ofrecemos una herramienta intuitiva y fácil de usar, diseñada
+        especialmente para guiarte a través de cada sesión de entrenamiento.
+      </div>
     </div>
   );
 }
